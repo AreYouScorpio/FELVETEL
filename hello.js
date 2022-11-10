57edc32bdaf855feffc76c5af03d0ca8a5311aed
AreYouScorpio <akos.schweger@gmail.com>
2022-11-10 10:18:33 +0100

    update

--- hello.js
+++ hello.js
@@ -1,19 +1,18 @@
 var Greeter = /** @class */ (function () {
-    // constructor(firstName: string, lastName: string) {
-    //     this.fullName = `${lastName} ${firstName}`
-    // }
-    function Greeter(nameObj) {
+    function Greeter(nameObj, skill) {
+        if (skill === void 0) { skill = "programming"; }
         this.fullName = "";
         this.skills = [
             { name: "programming", point: 33 }
         ];
         this.fullName = "".concat(nameObj.lastName, " ").concat(nameObj.firstName);
+        this.skill = skill;
     }
     Greeter.prototype.greet = function () {
         return "Hello, my name is ".concat(this.fullName, ".");
     };
     return Greeter;
 }());
-// let g: Greeter = new Greeter(`Jozsef`, `Cserko`);
-var g = new Greeter({ firstName: 'Jozsef', lastName: 'Cserko' });
+// let g: Greeter = new Greeter(`Jozsef`, `Cserko`); az idezojel is egyenes xD
+var g = new Greeter({ firstName: 'Jozsef', lastName: 'Cserko' }, "dancing");
 document.body.innerHTML = g.greet();
--- hello.ts
+++ hello.ts
@@ -1,6 +1,7 @@
 interface name {
     firstName: string;
-    lastName?: string;
+    lastName: string;
+    [propname: string] : string;
 }
 
 class Greeter {
@@ -11,15 +12,31 @@ class Greeter {
     // constructor(firstName: string, lastName: string) {
     //     this.fullName = `${lastName} ${firstName}`
     // }
-    constructor(nameObj: name) {
-        this.fullName = `${nameObj.lastName} ${nameObj.firstName}`
+
+
+    // readonly skill : string;
+    // constructor(nameObj: name, skill: string = "programming") {
+    //     this.fullName = `${nameObj.lastName} ${nameObj.firstName}`;
+    //     this.skill = skill;
+    // }
+
+    readonly skill : string;
+    constructor(nameObj: name, skill: string = "programming") {
+        this.fullName = `${nameObj.lastName} ${nameObj.firstName}`;
+        this.skill = skill;
     }
+
 
     greet() {
         return `Hello, my name is ${this.fullName}.`;
     }
 }
 
-// let g: Greeter = new Greeter(`Jozsef`, `Cserko`);
-let g: Greeter = new Greeter({firstName: 'Jozsef', lastName:'Cserko'});
+// let g: Greeter = new Greeter(`Jozsef`, `Cserko`); az idezojel is egyenes xD
+// let g: Greeter = new Greeter({firstName: 'Jozsef', lastName:'Cserko'}, "dancing");
+// document.body.innerHTML = g.greet();
+
+//nameParam erteke a name interface alapjan, kiemeljuk:
+let nameParam: name = {firstName: 'Jozsef', lastName:'Cserko', age:"22"};
+let g: Greeter = new Greeter(nameParam, "dancing");
 document.body.innerHTML = g.greet();
